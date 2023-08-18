c157fee1c231339566cd72f8f570d15ae038f8ad
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-08-19 01:50:24 +0300

    summary

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const gallery = document.querySelector(".gallery");
-const element = createElement(galleryItems);
 
-gallery.insertAdjacentHTML("beforeend", element);
+document.addEventListener("DOMContentLoaded", () => {
+  const galleryEl = document.querySelector(".gallery");
+  const element = createElement(galleryItems);
 
-function createElement(galleryItems) {
-  const arrayPhoto = galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-  <a class="gallery__link" href="${original} ">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-  </a>
-</li>`;
-    })
-    .join("");
-  return arrayPhoto;
-}
+  galleryEl.insertAdjacentHTML("beforeend", element);
 
-gallery.addEventListener("click", onPhotoGalleryClick);
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionDelay: 250,
+    captionsData: "alt",
+  });
 
-function onPhotoGalleryClick(e) {
-  e.preventDefault();
-  const targetPhoto = e.target.classList.contains("gallery__image");
-  if (!targetPhoto) {
-    return;
+  console.log(lightbox);
+  function createElement(galleryItems) {
+    const arrayPhoto = galleryItems
+      .map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+                             <a class="gallery__link" href="${original}">
+                                <img class="gallery__image" src="${preview}" alt="${description}" />
+                             </a>
+                          </li>`;
+      })
+      .join("");
+    return arrayPhoto;
   }
-  console.log(e.currentTarget.classList.contains("gallery__link"));
-}
+});
